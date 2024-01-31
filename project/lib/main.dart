7a7b29036b75c6c0f3a44af956efe61500f72f72
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-01-31 17:36:10 +0700

    button

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project/lession/button.dart';
 import 'package:project/lession/container.dart';
 import 'package:project/lession/navbar.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Mycontainer(),
+      home: const Mybutton(),
     );
   }
 }
