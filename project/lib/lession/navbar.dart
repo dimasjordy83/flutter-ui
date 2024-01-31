f4553822f109579b3b0554da09d0278060948a18
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-01-31 17:26:22 +0700

    text and navbar

---
+++ project/lib/lession/navbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Mynavbar extends StatelessWidget {
+  const Mynavbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Coding Flutter - Text"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
