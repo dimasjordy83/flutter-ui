90ee5fa4b6b4837bb7f313bc4b3879fa52a92579
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-02-01 11:37:01 +0700

    silver app

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -26,6 +26,7 @@ import 'package:project/lession/navigator/bottom.dart';
 import 'package:project/lession/navigator/drawer.dart';
 import 'package:project/lession/navigator/pageone.dart';
 import 'package:project/lession/navigator/pop.dart';
+import 'package:project/lession/navigator/silverapp.dart';
 import 'package:project/lession/navigator/tabbar.dart';
 
 void main() {
@@ -44,7 +45,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Mydrawer(),
+      home: Mysilver(),
     );
   }
 }
